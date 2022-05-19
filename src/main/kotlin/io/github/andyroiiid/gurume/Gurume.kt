76d18a270a0d51ed5d54906509f59dc362daf17c
Andrew Huang <andyroiiid@live.com>
2022-05-18 22:38:38 -0500

    cooked_rice

--- src/main/kotlin/io/github/andyroiiid/gurume/Gurume.kt
+++ src/main/kotlin/io/github/andyroiiid/gurume/Gurume.kt
@@ -2,12 +2,8 @@ package io.github.andyroiiid.gurume
 
 import io.github.andyroiiid.gurume.block.RiceCropBlock
 import net.fabricmc.api.ModInitializer
-import net.minecraft.block.AbstractBlock
-import net.minecraft.block.ComposterBlock
-import net.minecraft.block.Material
-import net.minecraft.item.AliasedBlockItem
-import net.minecraft.item.Item
-import net.minecraft.item.ItemGroup
+import net.minecraft.block.*
+import net.minecraft.item.*
 import net.minecraft.sound.BlockSoundGroup
 import net.minecraft.util.Identifier
 import net.minecraft.util.registry.Registry
@@ -38,18 +34,30 @@ object Gurume : ModInitializer {
             .group(ItemGroup.MISC)
     )
 
+    private val DEFAULT_FOOD_COMPONENT = FoodComponent.Builder().hunger(5).saturationModifier(0.6F).build()
+
+    private val COOKED_RICE_ITEM = Item(
+        Item.Settings()
+            .food(DEFAULT_FOOD_COMPONENT)
+            .group(ItemGroup.FOOD)
+    )
+
     override fun onInitialize() {
-        Logger.info("Registering items...")
+        Logger.info("Registering blocks...")
 
         Registry.register(Registry.BLOCK, Identifier("gurume", "rice_crop"), RICE_CROP_BLOCK)
 
+        Logger.info("Registering items...")
+
         Registry.register(Registry.ITEM, Identifier("gurume", "rice_seeds"), RICE_SEEDS_ITEM)
         Registry.register(Registry.ITEM, Identifier("gurume", "rice"), RICE_ITEM)
+        Registry.register(Registry.ITEM, Identifier("gurume", "cooked_rice"), COOKED_RICE_ITEM)
 
         Logger.info("Registering compostable items...")
 
         registerCompostableItem(0.3f, RICE_SEEDS_ITEM)
         registerCompostableItem(0.65f, RICE_ITEM)
+        registerCompostableItem(1.0f, COOKED_RICE_ITEM)
     }
 
     private fun registerCompostableItem(levelIncreaseChance: Float, item: Item) {
